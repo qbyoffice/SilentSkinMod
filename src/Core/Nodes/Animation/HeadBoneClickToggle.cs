c269a6d73cf79d116534817ff6aa5b82746adf74
青白影 <142860251+qbyoffice@users.noreply.github.com>
2026-08-28 00:41:58 +0800

    Update HeadBoneClickToggle.cs

--- src/Core/Nodes/Animation/HeadBoneClickToggle.cs
+++ src/Core/Nodes/Animation/HeadBoneClickToggle.cs
@@ -13,6 +13,11 @@ public partial class HeadBoneClickToggle : Control
         "tougu mianju 0",
         "mianju yanjing 0",
         "mianju yanjing 1",
+        "mianju yanjing 2",
+        "mianju yanjing 3",
+        "mianju yanjing 4",
+        "mianju yanjing 5",
+        "mianju yanjing 6",
         "tougu mianju 2",
         "mianju yinying 0"
     };
